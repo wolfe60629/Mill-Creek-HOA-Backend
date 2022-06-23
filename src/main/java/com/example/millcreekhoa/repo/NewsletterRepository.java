05b76aacdadc1e4f3b9997a6ec206e23c2b5a6af
Jeremy Wolfe <jeremy.wolfe@gm.com>
2022-06-22 21:06:31 -0400

    Edit the query to grab all newsletters for performance reasons

--- src/main/java/com/example/millcreekhoa/repo/NewsletterRepository.java
+++ src/main/java/com/example/millcreekhoa/repo/NewsletterRepository.java
@@ -3,12 +3,14 @@ package com.example.millcreekhoa.repo;
 import com.example.millcreekhoa.entity.DocumentEntity;
 import com.example.millcreekhoa.entity.NewsletterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface NewsletterRepository extends JpaRepository<NewsletterEntity, Integer> {
+    @Query(value = "select id,crt_ts,category,friendly_name,name,description,'' as item from newsletter order by crt_ts desc ", nativeQuery = true)
     List<NewsletterEntity> findAllByOrderByCreateTimestampDesc();
 
 
